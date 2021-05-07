a987b927dbd39d87975ebdcbabfa19d2a234b572
Wessel Konstantinov <wes_kon1@hotmail.com>
2021-05-07 16:41:39 +0200

    Load data into state

--- src/App.js
+++ src/App.js
@@ -1,12 +1,40 @@
+import { csv } from 'd3';
+import React, { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Container from '@material-ui/core/Container';
 import Menu from './components/Menu';
 import HomePage from './components/HomePage';
 import StudentsListPage from './components/StudentsListPage';
 import AssignmentsListPage from './components/AssignmentsListPage';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
+  const [students, setStudents] = useState([]);
+  const [evaluations, setEvaluations] = useState([]);
+
+  useEffect(() => {
+    const fetchStudents = async () => {
+      const response = await fetch('./data/students-data.json');
+      const data = await response.json();
+      setStudents(data.results);
+    };
+    fetchStudents();
+  }, []);
+
+  useEffect(() => {
+    const fetchEvaluations = async () => {
+      const results = await csv('./data/evaluations-data.csv');
+      const data = results.map(result => ({
+        student: result.student,
+        assignment: result.assignment,
+        difficulty: +result.difficulty,
+        enjoyability: +result.enjoyability,
+      }));
+      setEvaluations(data);
+    };
+    fetchEvaluations();
+  }, []);
+
   return (
     <Router>
       <div className="App">
@@ -18,7 +46,7 @@ function App() {
               <HomePage />
             </Route>
             <Route exact path="/students">
-              <StudentsListPage />
+              <StudentsListPage students={students} />
             </Route>
             <Route exact path="/assignments">
               <AssignmentsListPage />
