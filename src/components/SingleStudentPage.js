8184872e9b99413cfce83d6a727bf0d1cea6fa6f
Wessel Konstantinov <wes_kon1@hotmail.com>
2021-05-09 15:06:06 +0200

    Include Chart component

--- src/components/SingleStudentPage.js
+++ src/components/SingleStudentPage.js
@@ -6,6 +6,7 @@ import Divider from '@material-ui/core/Divider';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
+import AssignmentsChart from './AssignmentsChart';
 import { useParams } from 'react-router-dom';
 
 const useStyles = makeStyles(theme => ({
@@ -23,58 +24,71 @@ const useStyles = makeStyles(theme => ({
     },
 }));
 
-function SingleStudentPage({ students }) {
+function SingleStudentPage({ students, evaluations }) {
     const classes = useStyles();
     const { name } = useParams();
     const student = students.find(student => student.name.first === name);
+    const chartData = evaluations.filter(evaluation => evaluation.student === name);
 
     return (
-        <Paper className={classes.root} elevation={3}>
-            <Typography
-                component="h2"
-                variant="h3"
-                align="center"
-            >
-                Student Details
+        <>
+            <Paper className={classes.root} elevation={3}>
+                <Typography
+                    component="h2"
+                    variant="h3"
+                    align="center"
+                >
+                    Student Details
             </Typography>
-            <Avatar
-                className={classes.profilePicture}
-                variant="rounded"
-                alt={`${student.name.first} ${student.name.last}`}
-                src={student.picture.large}
-            />
-            <Divider />
-            <List>
-                <ListItem divider>
-                    <ListItemText
-                        align="center"
-                        primary="Full name"
-                        secondary={`${student.name.first} ${student.name.last}`}
-                    />
-                </ListItem>
-                <ListItem divider>
-                    <ListItemText
-                        align="center"
-                        primary="Age"
-                        secondary={student.dob.age}
-                    />
-                </ListItem>
-                <ListItem divider>
-                    <ListItemText
-                        align="center"
-                        primary="Email"
-                        secondary={student.email}
-                    />
-                </ListItem>
-                <ListItem>
-                    <ListItemText
-                        align="center"
-                        primary="Phone number"
-                        secondary={student.cell}
-                    />
-                </ListItem>
-            </List>
-        </Paper>
+                <Avatar
+                    className={classes.profilePicture}
+                    variant="rounded"
+                    alt={`${student.name.first} ${student.name.last}`}
+                    src={student.picture.large}
+                />
+                <Divider />
+                <List>
+                    <ListItem divider>
+                        <ListItemText
+                            align="center"
+                            primary="Full name"
+                            secondary={`${student.name.first} ${student.name.last}`}
+                        />
+                    </ListItem>
+                    <ListItem divider>
+                        <ListItemText
+                            align="center"
+                            primary="Age"
+                            secondary={student.dob.age}
+                        />
+                    </ListItem>
+                    <ListItem divider>
+                        <ListItemText
+                            align="center"
+                            primary="Email"
+                            secondary={student.email}
+                        />
+                    </ListItem>
+                    <ListItem>
+                        <ListItemText
+                            align="center"
+                            primary="Phone number"
+                            secondary={student.cell}
+                        />
+                    </ListItem>
+                </List>
+            </Paper>
+            <Paper className={classes.root} elevation={3}>
+                <Typography
+                    component="h2"
+                    variant="h3"
+                    align="center"
+                >
+                    Student Ratings
+            </Typography>
+                <AssignmentsChart chartData={chartData} />
+            </Paper>
+        </>
     );
 }
 
